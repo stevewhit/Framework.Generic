915f75081ca59c714fc6a30554fdf10ae629e66e
stevewhit <swhit114@gmail.com>
2020-03-24 15:44:10 -0400

    Updated all nuget packages; Added SystemTime object to enhance unit testing;

---
+++ Framework.Generic.Tests/Utility/SystemTimeTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Framework.Generic.Utility;
+
+namespace Framework.Generic.Tests.Utility
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class SystemTimeTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+        }
+
+        #region Testing void SetDateTime(DateTime dateTimeNow)
+
+        [TestMethod]
+        public void SetDateTime_WithValidDateTime_SetsDateTime()
+        {
+            // Arrange
+            var fakeTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 30, 0);
+
+            // Act
+            SystemTime.SetDateTime(fakeTime);
+
+            // Assert
+            Assert.IsTrue(SystemTime.Now() == fakeTime);
+        }
+
+        #endregion
+        #region Testing void ResetDateTime()
+
+        [TestMethod]
+        public void ResetDateTime_ResetsDateTime()
+        {
+            // Arrange
+            var fakeTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 30, 0);
+            SystemTime.SetDateTime(fakeTime);
+
+            // Act
+            SystemTime.ResetDateTime();
+
+            // Assert
+            Assert.IsTrue(SystemTime.Now() != fakeTime);
+        }
+
+        #endregion  
+    }
+}
---
+++ Framework.Generic/Utility/SystemTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Framework.Generic.Utility
+{
+    public static class SystemTime
+    {
+        /// <summary>
+        /// Returns either the current time, or the overridden value.
+        /// </summary>
+        public static Func<DateTime> Now = () => DateTime.Now;
+
+        /// <summary>
+        /// Sets the time to return when SystemTime.Now() is called.
+        /// </summary>
+        /// <param name="dateTimeNow">The time to return when SystemTime.Now() is called.</param>
+        public static void SetDateTime(DateTime dateTimeNow)
+        {
+            Now = () => dateTimeNow;
+        }
+
+        /// <summary>
+        /// Resets SystemTime.Now() to return DateTime.Now.
+        /// </summary>
+        public static void ResetDateTime()
+        {
+            Now = () => DateTime.Now;
+        }
+    }
+}
